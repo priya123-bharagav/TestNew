e8d7a875ddd4e5bd4cff4c107d841d63231ab691
priya123-bharagav <bhargavapriya16@gmail.com>
2021-08-13 00:13:58 +0530

    replaced two nested for loops with while and stream foreach

--- src/main/java/main/Read.java
+++ src/main/java/main/Read.java
@@ -7,56 +7,58 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Read {
 
 	public static void main(String[] args) {
-		String arrayOfWords[] = null;
-		List<String[]> listOfReplacedStrings = new ArrayList();
+
 		Map<String, String> properties = getKeyandValue();
 		String filePath = "src/test/resources/textfile.txt";
-		String content = null;
-
 		try {
-			content = Files.lines(Paths.get(filePath)).collect(Collectors.joining());
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			List<String> content = Arrays
+					.asList(Files.lines(Paths.get(filePath)).collect(Collectors.joining()).split("\\W+"));
+
+			Iterator<Map.Entry<String, String>> itr = properties.entrySet().iterator();
+
+			while (itr.hasNext()) {
+				AtomicInteger counter = new AtomicInteger(0);
+				Map.Entry<String, String> entry = itr.next();
+				content.stream().forEach(item -> {
+					if (item.equals(entry.getKey())) {
+						String key = entry.getKey();
+						content.set(content.indexOf(key), entry.getValue());
+						counter.getAndIncrement();
+					}
+				});
+				System.out.println(entry.getKey().toUpperCase() + " " + "was replaced" + " " + counter.get() + " " + "times");
+				writeIntoFile(content);
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
 		}
 
-		arrayOfWords = content.split("\\W+");
-		for (String key : properties.keySet()) {
-			int countKey = 0;
-			for (int i = 0; i < arrayOfWords.length; i++) {
-				if (arrayOfWords[i].equals(key)) {
-					arrayOfWords[i] = properties.get(key);
-					countKey++;
-				}
-			}
-			System.out.println(key.toUpperCase() + " " + "is replaced" + " " + countKey + " " + "time");
-		}
-		listOfReplacedStrings.add(arrayOfWords);
-		writeIntoFile(listOfReplacedStrings);
 	}
 
 //Method to write replaced content into new output file.
-	public static void writeIntoFile(List<String[]> listOfArray) {
+	public static void writeIntoFile(List<String> content) {
 		String fileName = "output.txt";
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(fileName)));
-			for (String[] eachArray : listOfArray) {
-				for (String eachString : eachArray) {
-					if (eachString.matches("^[a-zA-Z]*$"))
-						eachString = eachString + " ";
-					writer.write(eachString);
-				}
+			for (String eachString : content) {
+				if (eachString.matches("^[a-zA-Z]*$"))
+					eachString = eachString + " ";
+				writer.write(eachString);
 			}
 			writer.close();
 
